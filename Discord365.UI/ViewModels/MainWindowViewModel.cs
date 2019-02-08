4d5d632938b3259ed084246fa79817fc95a0cd0f
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-02-08 17:45:09 +0200

    Move UI to another project

---
+++ Discord365.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discord365.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
--- Discord365/Program.cs
+++ Discord365/Program.cs
@@ -2,27 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+
 
 namespace Discord365
 {
     class Program
     {
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
-            if(Environment.OSVersion.Version.Major <= 5 || // xp or earlier
-                (Environment.OSVersion.Version.Minor == 0 && Environment.OSVersion.Version.Major == 6)) // vista
-            {
-                var msg = System.Windows.MessageBox.Show("Your OS is not supported by Discord365. The minimal supported version is Windows 7. Do you want to continue?", "Discord365", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Warning);
-
-                if(msg != System.Windows.MessageBoxResult.Yes)
-                {
-                    return 1;
-                }
-            }
-
-            Discord365.Core.App.RunCore(args);
-
-            return 0;
+            UI.Program.Main(args);
         }
+
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<UI.App>()
+                .UsePlatformDetect()
+                .LogToDebug();
     }
 }
