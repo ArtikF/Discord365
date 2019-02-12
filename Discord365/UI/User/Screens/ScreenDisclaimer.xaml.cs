caa63a701c147be64961ba7fa11bdd0e0fd95a30
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-02-12 16:39:27 +0200

    Exlude DIscord.Net projects from build, add some stub screens

---
+++ Discord365/UI/User/Screens/ScreenDisclaimer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Discord365.UI.User.Screens
+{
+    /// <summary>
+    /// Interaction logic for ScreenDisclaimer.xaml
+    /// </summary>
+    public partial class ScreenDisclaimer : UserControl
+    {
+        public ScreenDisclaimer()
+        {
+            InitializeComponent();
+        }
+    }
+}
