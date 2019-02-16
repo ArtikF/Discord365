c062d6ed4f3f3b2e03c69151f26450620d565e7a
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-02-16 21:57:04 +0200

    [ImageViewer] Implement ImageViewer

---
+++ Discord365/UI/ImageViewer/ImageInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Discord365.UI.ImageViewer
+{
+    public class ImageInfo
+    {
+        public string source = "";
+
+        public ImageInfo(string sourceUri)
+        {
+            source = sourceUri;
+        }
+
+        public ImageSource ToImageSource()
+        {
+            return BitmapFrame.Create(new Uri(source, UriKind.Absolute));
+        }
+    }
+}
---
+++ Discord365/UI/ImageViewer/ImageViewer.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Discord365.UI.ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for ImageViewer.xaml
+    /// </summary>
+    public partial class ImageViewer : Window
+    {
+        public ImageViewer()
+        {
+            InitializeComponent();
+        }
+
+        public List<ImageInfo> Album = new List<ImageInfo>();
+        private int imageIndex = 0;
+        public int ImageIndex
+        {
+            get => imageIndex;
+            set => Select(value);
+        }
+
+        public void AddImage(ImageInfo i)
+        {
+            Album.Add(i);
+
+            if (Album.Count == 1)
+                Select(GetImageIndex(i));
+        }
+
+        public int GetImageIndex(ImageInfo info)
+        {
+            for(int i =0 ;i < Album.Count; i++)
+            {
+                if (Album[i] == info)
+                    return i;
+            }
+
+            return 0;
+        }
+
+        public void Select(int Index)
+        {
+            imageIndex = Index;
+
+            ImageCanvas.Children.Clear();
+            ImageCanvas.Children.Add(new Image() { Source = Album[Index].ToImageSource() });
+        }
+
+        private void ClickToCloseGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
