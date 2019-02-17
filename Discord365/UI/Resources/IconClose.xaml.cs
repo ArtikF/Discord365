822a8df854ebbdb859fb1c9aab8ccd036d9daa7a
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-02-17 14:38:43 +0200

    [UI] Implement buttons style, add close button to resources and to custom ctrl

---
+++ Discord365/UI/Resources/IconClose.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Discord365.UI.Resources
+{
+    /// <summary>
+    /// Interaction logic for IconClose.xaml
+    /// </summary>
+    public partial class IconClose : UserControl
+    {
+        public IconClose()
+        {
+            InitializeComponent();
+        }
+    }
+}
