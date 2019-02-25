e7653be4ef6c7790eeb211ecc08cd36de4ae3d4e
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-02-25 18:06:55 +0200

    Unhandled exceptions handler

--- Discord365/App.xaml.cs
+++ Discord365/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,8 @@ namespace Discord365
     /// </summary>
     public partial class App : Application
     {
+        public const string ErrorLogFile = "discord365_crash.log";
+
         public static UI.LoginWindow LoginWnd;
         public static UI.MainClientWnd MainWnd;
         public static UI.AppWindow AppWnd;
@@ -28,8 +31,40 @@ namespace Discord365
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+
             AppWnd = new UI.AppWindow();
             AppWnd.ShowDialog();
+        }
+
+        private void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+                return;
+
+            try
+            {
+                using(StreamWriter w = new StreamWriter(ErrorLogFile))
+                {
+                    w.WriteLine("Unhandled Exception Happened at " + DateTime.Now.ToString());
+                    w.WriteLine("Send this file there: https://github.com/discord365/Discord365/issues/new");
+                    w.WriteLine();
+                    w.WriteLine("Author: " + Environment.UserName);
+                    w.WriteLine("Receiver: https://github.com/discord365/Discord365/issues");
+                    w.WriteLine("Topic: " + ((Exception)e.ExceptionObject).Message);
+                    w.WriteLine();
+                    w.WriteLine(((Exception)e.ExceptionObject).ToString());
+                }
+
+                var dialog = MessageBox.Show($"Discord 365 just made a big WHOOPSIE.\r\n\r\nCrash information and instructions are saved in {ErrorLogFile}.\r\n\r\nPress 'Yes' to close the application and open this file.\r\nPress 'No' if you want to start the default debugger.", "Discord 365 Crashed", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+
+                if (dialog == MessageBoxResult.Yes)
+                {
+                    System.Diagnostics.Process.Start(ErrorLogFile);
+                    Environment.Exit(1);
+                }
+            }
+            catch { }
         }
     }
 }
